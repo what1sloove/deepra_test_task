8c6944d51a4a7d19b1f229209279ccb9eacfb778
what1sloove <vladis4ulove@.com>
2024-10-09 22:51:47 +0300

    Add query parameters.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+from typing import Optional
 from fastapi import FastAPI
-
-from schemas import MessageModel
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root(data: MessageModel):
-    return f"Hello {data.name}! {data.message}"
+def read_root(name: Optional[str] = "Recruto", message: Optional[str] = "Давай дружить"):
+    return {f"Hello {name}! {message}"}
--- schemas.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-from pydantic import BaseModel
-
-
-class MessageModel(BaseModel):
-    name: Optional[str] = "Recruto"
-    message: Optional[str] = "Давай дружить"
